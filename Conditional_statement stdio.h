fa360bfb31ca236f9f473235b519817261dd6eda
BOWEN YIP WEI DE <98448831+bowenyipweide@users.noreply.github.com>
2022-10-10 08:25:46 +0800

    Update Conditional_statement stdio.h

--- Conditional_statement stdio.h
+++ Conditional_statement stdio.h
@@ -1,2 +1,48 @@
 #include <stdio.h>
+#include <cs50.h>
 
+if (boolean-expression)
+{
+  
+}
+else 
+{
+  
+}
+
+#Nesting conditional 
+if (boolean-expr1)
+{
+  // First branch
+}
+else if (boolean-expr2)
+{
+  // Second branch
+}
+else if (boolean-expr3)
+{
+  // Third branch 
+}
+else 
+{
+  // Fourth branch 
+}
+
+#Important to put a break, you will fall through
+int x = Getint();
+switch(x)
+{
+  case 1:
+    printf("One!\n");
+    break;
+  case 2:
+    printf("Two!\n");
+    break;
+   case 3:
+    printf("Three!\n");
+    break;
+   defalut:
+    printf("sorry! \n");
+}
+    
+    
