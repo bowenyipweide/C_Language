9ab1ba08d379e2a233bc0365ce067e2a28cb26b3
BOWEN YIP WEI DE <98448831+bowenyipweide@users.noreply.github.com>
2022-10-10 08:05:55 +0800

    Update Operators stdio.h

--- Operators stdio.h
+++ Operators stdio.h
@@ -1,2 +1,49 @@
 #include <stdio.h>
 
+#Arithmetic Operators 
+In order to manipulate and work with variables and values in C, we are have a number of operators at our disposal.
+
+#example 
+int x = y + 1;
+x = x * 5;
+int m = 13 % 4; // m is now 1
+
+x = x *5;
+x *= ;
+
+#shorthand for incrementing or decrementing
+x++;
+x--;
+
+#Boolean Expression 
+All boolean expression in C evalulate to 1 or 2 possible values, true or false.
+  deciding which branch in a conditional to take 
+In C, every nonzeror is equivalent to true, and zero is false (logical and relational operators)
+  #Logical operators (&&)
+  true && true = true 
+  true && false = false
+  false && true = false
+  false && false = false
+  
+  #Logical operators (||)
+  if and only if at least one operand is true, otherwise false.
+  true || true = true 
+  true || false = true
+  false || true = true
+  false || false = false
+    
+  #Logical operators (!)
+  inverts the value of its operand.
+  true != false
+  false != true
+    
+  #relational operators
+  Less than (x < Y)
+  Less than or equal to (x <= y)
+  Greater than (x > y)
+  Greater than or equal to (x >= y)
+  
+  Equality (x == y)
+  Inequality (x != y)
+  #Common mistake, assignment operators (=), equality operators (==)
+    
