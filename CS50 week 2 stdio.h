800d2509a4c16c837ce1dcb1942b79fbbf1ae3fb
BOWEN YIP WEI DE <98448831+bowenyipweide@users.noreply.github.com>
2022-10-21 20:43:46 +0800

    Update and rename CS50 week 2 <stdio.h> to CS50 week 2 stdio.h

---
+++ CS50 week 2 stdio.h
@@ -0,0 +1,10 @@
+<stdio.h> 
+
+#Why call it a black box? 
+if we aren't writing the function ourselves, we don't need to know the underlying implementation.
+
+#functions declaration:
+  return-type name(argument-list);
+The "return-type" is what kind of variuable the function will output. 
+The "name" is what you want to call your functions.
+The "Argument-list" is the comma-seperated set of input to your functions, each of which has a type and a name.
